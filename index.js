83c7b886d2f04f039f076ba503e93d83ce1c5acf
Ardhi Lukianto <ardhi@lukianto.com>
2017-12-05 11:23:30 +0700

    aku urip

---
+++ index.js
@@ -0,0 +1,98 @@
+'use strict'
+
+const _ = require('lodash')
+
+class Dab {
+  constructor (options = {}) {
+    this.client = null
+    this._ = _
+    this.options = {}
+    this.setOptions(options)
+  }
+
+  setOptions (options = {}) {
+    options.limit = options.limit || 25
+    options.options = options.options || {}
+    this.options = this._.merge(this.options, options)
+  }
+
+  defConverter (doc = {}) {
+    let newDoc = _.cloneDeep(doc),
+      idSrc = this.options.idSrc || '_id',
+      idDest = this.options.idDest || idSrc
+    if (idSrc !== idDest && _.has(newDoc, idSrc)) {
+      newDoc[idDest] = newDoc[idSrc]
+      delete newDoc[idSrc]
+    }
+    return newDoc
+  }
+
+  convertDoc (data, param = {}) {
+    let isArray = _.isArray(data),
+      result = isArray ? _.cloneDeep(data) : [_.cloneDeep(data)]
+    _.each(result, (r, i) => {
+      result[i] = this.defConverter(r)
+      if (typeof param.converter === 'function')
+        result[i] = param.converter(result[i])
+    })
+    return isArray ? result : result[0]
+  }
+
+  setClient (options) {
+  }
+
+  getClient () {
+    if (!this.client) this.setClient()
+    return this.client    
+  }
+
+  sanitize (params, body = {}) {
+    body = _.cloneDeep(body)
+    params = typeof params === 'string' ? { dbName: params } : (params || {})
+    return [params, body]
+  }
+
+  delFakeGetReal (body) {
+    if (body[this.options.idDest] && this.options.idDest !== this.options.idSrc) {
+      body[this.options.idSrc] = body[this.options.idDest]
+      delete body[this.options.idDest]
+    }
+    let id = body[this.options.idSrc]
+    return [body, id]    
+  }
+
+  _notImplemented () {
+    return Promise.reject(new Error('Not implemented'))
+  }
+
+  find (params) {
+    return this._notImplemented()
+  }
+
+  findOne (id, params) {
+    return this._notImplemented()
+  }
+
+  create (body, params) {
+    return this._notImplemented()
+  }
+
+  update (id, body, params) {
+    return this._notImplemented()
+  }
+
+  remove (id, params) {
+    return this._notImplemented()
+  }
+
+  delete (id, params) {
+    return this.remove(id, params)
+  }
+
+  destroy (id, params) {
+    return this.remove(id, params)
+  }
+  
+}
+
+module.exports = Dab
