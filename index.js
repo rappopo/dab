585ab45606af68768650f82dfbf480c7a9eb2077
Ardhi Lukianto <ardhi@lukianto.com>
2017-12-05 20:34:38 +0700

    new interface

--- index.js
+++ index.js
@@ -10,32 +10,14 @@ class Dab {
     this.setOptions(options)
   }
 
+  /**
+  * Common
+  */
+
   setOptions (options = {}) {
     options.limit = options.limit || 25
     options.options = options.options || {}
     this.options = this._.merge(this.options, options)
-  }
-
-  defConverter (doc = {}) {
-    let newDoc = _.cloneDeep(doc),
-      idSrc = this.options.idSrc || '_id',
-      idDest = this.options.idDest || idSrc
-    if (idSrc !== idDest && _.has(newDoc, idSrc)) {
-      newDoc[idDest] = newDoc[idSrc]
-      delete newDoc[idSrc]
-    }
-    return newDoc
-  }
-
-  convertDoc (data, param = {}) {
-    let isArray = _.isArray(data),
-      result = isArray ? _.cloneDeep(data) : [_.cloneDeep(data)]
-    _.each(result, (r, i) => {
-      result[i] = this.defConverter(r)
-      if (typeof param.converter === 'function')
-        result[i] = param.converter(result[i])
-    })
-    return isArray ? result : result[0]
   }
 
   setClient (options) {
@@ -44,6 +26,21 @@ class Dab {
   getClient () {
     if (!this.client) this.setClient()
     return this.client    
+  }
+
+  /**
+  * Helper
+  */
+
+  convertDoc (data, param = {}) {
+    let isArray = _.isArray(data),
+      result = isArray ? _.cloneDeep(data) : [_.cloneDeep(data)]
+    _.each(result, (r, i) => {
+      result[i] = this._defConverter(r)
+      if (typeof param.converter === 'function')
+        result[i] = param.converter(result[i])
+    })
+    return isArray ? result : result[0]
   }
 
   sanitize (params, body = {}) {
@@ -61,9 +58,28 @@ class Dab {
     return [body, id]    
   }
 
+  /**
+  * Privates
+  */
+
+  _defConverter (doc = {}) {
+    let newDoc = _.cloneDeep(doc),
+      idSrc = this.options.idSrc || '_id',
+      idDest = this.options.idDest || idSrc
+    if (idSrc !== idDest && _.has(newDoc, idSrc)) {
+      newDoc[idDest] = newDoc[idSrc]
+      delete newDoc[idSrc]
+    }
+    return newDoc
+  }
+
   _notImplemented () {
     return Promise.reject(new Error('Not implemented'))
   }
+
+  /**
+  * Main methods
+  */
 
   find (params) {
     return this._notImplemented()
@@ -84,6 +100,26 @@ class Dab {
   remove (id, params) {
     return this._notImplemented()
   }
+
+  /**
+  * Bulk
+  */
+
+  bulkCreate(body, params) {
+    return this._notImplemented()    
+  }
+
+  bulkUpdate(body, params) {
+    return this._notImplemented()    
+  }
+
+  bulkRemove(body, params) {
+    return this._notImplemented()    
+  }
+
+  /**
+  * Aliases
+  */
 
   delete (id, params) {
     return this.remove(id, params)
